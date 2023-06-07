e6c2740c8a6928a48a03ed7c34c04f1ad53c4e7d
Ulich Igor <ylichigor@gmail.com>
2023-06-07 08:53:25 +0300

    port: 3000

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,40 @@
+const path = require('path');
+
 export const module = {
-  devServer: {
-    contentBase: './public',
-    port: 9000
+  entry: path.join(__dirname, "src", "index.js"),
+  output: {
+    path: path.join(__dirname, "public"),
+  
   },
-  rules: [
-    {
-      test: /\.s[ac]ss$/i,
-      use: [
-        "style-loader",
-        "css-loader",
-        {
-          loader: "sass-loader",
-          options: {
-            implementation: require("sass"),
-            sassOptions: {
-              fiber: false,
-            },
+  devServer: {
+    port: 9000,  
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    client: {
+      overlay: {
+        errors: true,
+        warnings: false,
+        runtimeErrors: true,
+      },
+    },
+  },
+  rules: [{
+    test: /\.s[ac]ss$/i,
+    use: [
+      "style-loader",
+      "css-loader",
+      {
+        loader: "sass-loader",
+        options: {
+          implementation: require("sass"),
+          sassOptions: {
+            fiber: false,
           },
         },
-      ],
-    },
+      },
+    ],
+  },
+
   ],
 };
